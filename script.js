ca7baaa02f5727b20aefaa2ba4fd3585c747e36d
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2022-11-10 13:00:34 +0100

    feat: added basic operations

--- script.js
+++ script.js
@@ -20,20 +20,72 @@ keyboard.addEventListener("click", function handleButton(event) {
 	}
 });
 
-let displayValue = 0;
 let memoryValue = 0;
 let nextOperation = null;
+let lastButton = null;
 
-function specialOperation(operation) {}
+function specialOperation(operation) {
+	if (operation == "clear") {
+		memoryValue = 0;
+		nextOperation = null;
+		lastButton = null;
+		display.innerHTML = "0";
+	}
+	if (operation == "backspace") {
+		display.innerHTML = display.innerHTML.slice(0, -1);
+	}
+	if (operation == "sign") {
+	}
+}
 
-function operate(operator) {}
-
-function addNumber(number) {
-	if (display.innerHTML == "0") {
-		if (number != 0) {
-			display.innerHTML = String(number);
+function operate(operator) {
+	if (nextOperation == null) {
+		if (operator != "result") {
+			nextOperation = operator;
+			memoryValue = Number(display.innerHTML);
 		}
 	} else {
-		display.innerHTML += String(number);
+		memoryValue = calculate(
+			memoryValue,
+			Number(display.innerHTML),
+			nextOperation
+		);
+		if (operator != "result") {
+			nextOperation == operator;
+		}
+		display.innerHTML = memoryValue;
+	}
+
+	lastButton = "operator";
+}
+
+function addNumber(number) {
+	if (lastButton == "operator") {
+		display.innerHTML = String(number);
+	} else {
+		if (display.innerHTML == "0") {
+			if (number != 0) {
+				display.innerHTML = String(number);
+			}
+		} else {
+			display.innerHTML += String(number);
+		}
+	}
+}
+
+function calculate(number1, number2, operator) {
+	if (operator == "percent") {
+	}
+	if (operator == "divide") {
+		return number1 / number2;
+	}
+	if (operator == "multiply") {
+		return number1 * number2;
+	}
+	if (operator == "subtract") {
+		return number1 - number2;
+	}
+	if (operator == "add") {
+		return number1 + number2;
 	}
 }
